41fae526e0d5af42cb5a4f8e741ccb06787e352e
luigi <luigi.gil@gmail.com>
2024-05-02 23:05:41 -0300

    fix: server address on main

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,10 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let address = format!("127.0.0.1:{}", configuration.application.port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind port");
     run(listener, connection_pool)?.await
 }
