c30fb618349d21b088ea315f27fd1366590e49a5
luigi <luigi.gil@gmail.com>
2024-05-13 19:40:54 -0300

    feat: subscriptions_confirm route

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
+use crate::routes::confirm;
 use crate::routes::health_check;
 use crate::routes::subscribe;
 
@@ -70,6 +71,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
